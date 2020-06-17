d7a4a848763a06d1bd68618b0bb67856f90123e2
aymen-91 <47060385+aymen-91@users.noreply.github.com>
2020-06-17 12:14:22 +0100

    Add files via upload

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const app=express()
+
+app.listen(5000,(err)=>{
+    if(err)
+    console.log('errr')
+    else console.log('is working')})
+let time ;
+time = new Date()
+hours =time.getHours()
+minutes=time.getMinutes()
+console.log(hours +','+ minutes)
+if(hours<17&&hours>12)
+{app.use(express.static('public'))
+}
+else {
+    app.get('/', (req,res)=>
+    res.send('Our office is not open now'))
+    app.get(`/contact.html`, (req,res)=>
+    res.send('Our office is not open now'))
+    app.get('/ourservices.html', (req,res)=>
+    res.send('Our office is not open now'))
+    app.get('/index.html', (req,res)=>
+    res.send('Our office is not open now'))
+}    
